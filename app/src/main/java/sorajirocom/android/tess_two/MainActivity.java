f170b164f4504795a29f1532472762fd7aa6ed2f
atusi muroya <atusi.08131995@gmail.com>
2018-09-05 15:02:49 +0900

    写真撮影作成

--- app/src/main/java/sorajirocom/android/tess_two/MainActivity.java
+++ app/src/main/java/sorajirocom/android/tess_two/MainActivity.java
@@ -3,10 +3,13 @@ package sorajirocom.android.tess_two;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
@@ -18,16 +21,19 @@ import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     static final String DEFAULT_LANGUAGE = "jpn";
+    static final int REQUEST_CODE_CAMERA = 1;
 
     String filepath;
     Bitmap bitmap;
     TessBaseAPI tessBaseAPI;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.konnitiwa);
+        imageView = (ImageView)findViewById(R.id.image_view);
+        //bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.konnitiwa);
 
         filepath = getFilesDir() + "/tesseract/";
 
@@ -86,5 +92,22 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+    public void camera(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_CODE_CAMERA);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode,resultCode,intent);
+        if(requestCode == REQUEST_CODE_CAMERA){
+            bitmap = (Bitmap) intent.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+
+
+        }else if(resultCode == RESULT_CANCELED ){
+            Toast.makeText(this,"CANCEL",Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
